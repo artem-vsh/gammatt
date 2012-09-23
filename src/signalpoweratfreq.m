b1b403f5c1f2b3d10792f6bcd67065a3dcf2d08b
Artem Shitov <artem.shitov@wikimart.ru>
2012-09-24 02:30:49 +0400

    add function to search for power at some frequency by averagins neighbour freqs

---
+++ src/signalpoweratfreq.m
@@ -0,0 +1,22 @@
+function [ Power ] = signalpoweratfreq( FreqByPower, Freq )
+%SIGNALPOWERATFREQ Find power at specified freq
+%   If FreqByPower is of form [ ... ; f p ; ... ], return p
+%   else iif FreqByPower is of form [ ... ; f-a p ; f+b q ; ... ], a,b > 0
+%   return (p-q)/2
+
+Exact = FreqByPower(:, 1) == Freq;
+if any(Exact)
+    Power = FreqByPower(Exact, 2);
+else
+    Deviations = FreqByPower(:, 1) - Freq;
+    Left = Deviations == max(Deviations(Deviations < 0));
+    Right = Deviations == min(Deviations(Deviations > 0));
+    if ~any(Left) || ~any(Right)
+        Power = [];
+    else
+        Power = (FreqByPower(Left, 2) + FreqByPower(Right, 2)) / 2;
+    end
+end
+
+end
+
