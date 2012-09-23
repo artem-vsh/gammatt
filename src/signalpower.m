e3e07254b7120da8a281a0db41c6ff0b1142a195
Artem Shitov <artem.shitov@wikimart.ru>
2012-09-24 00:46:50 +0400

    comments edited, cell mode enabled

--- src/signalpower.m
+++ src/signalpower.m
@@ -1,7 +1,10 @@
 function [ PowerByFrequency ] = signalpower( signal, Fs )
 %POWER Compute classical square power periodogram
 
+%% compute periodogram
 [ Power, W ] = periodogram(signal); % where W in rad/sample
+
+%% perform unit conversion
 % see http://en.wikipedia.org/w/index.php?oldid=498928359
 % for more information concerning rad vs hz relation
 %
@@ -12,6 +15,7 @@ function [ PowerByFrequency ] = signalpower( signal, Fs )
 % as 1 Hz = 2π rad, n Hz = n rad * 2π and n rad = (n / 2π) Hz
 Frequency = (W * Fs) / (2 * pi);
 
+%% return
 PowerByFrequency = [ Frequency Power ];
 
 end
