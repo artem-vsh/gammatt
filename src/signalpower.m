dfaf0cdf1497a2d4ad31d34bc591b87a3b513235
Artem Shitov <artem.shitov@wikimart.ru>
2012-09-24 01:44:01 +0400

    change rad/sample -> hz conversion to standard, fix power unit bug

--- src/signalpower.m
+++ src/signalpower.m
@@ -18,18 +18,7 @@ else
 end
 
 %% compute periodogram
-[ Power, W ] = periodogram(signal, window, lSignal); % W in rad/sample
-
-%% perform unit conversion
-% see http://en.wikipedia.org/w/index.php?oldid=498928359
-% for more information concerning rad vs hz relation
-%
-% see http://en.wikipedia.org/w/index.php?oldid=511747566
-% for more information on rad/sample -> cycles/sec aka hz converting
-%
-% W : rad/sample, Fs : samples/sec => W*Fs = rad/sec
-% as 1 Hz = 2π rad, n Hz = n rad * 2π and n rad = (n / 2π) Hz
-Frequency = (W * Fs) / (2 * pi);
+[ Power, Frequency ] = periodogram(signal, window, lSignal, Fs);
 
 %% return
 PowerByFrequency = [ Frequency Power ];
