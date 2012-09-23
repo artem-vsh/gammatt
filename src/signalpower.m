99b7f0ff4cc9389a4b78de7f5ac0d56d0f664f71
Artem Shitov <artem.shitov@wikimart.ru>
2012-09-24 01:16:18 +0400

    add windowing and nFFT overriding

--- src/signalpower.m
+++ src/signalpower.m
@@ -1,8 +1,24 @@
-function [ PowerByFrequency ] = signalpower( signal, Fs )
+function [ PowerByFrequency ] = signalpower( signal, Fs, window )
 %POWER Compute classical square power periodogram
+lSignal = length(signal);
+if nargin < 3
+    window = [];
+end
+
+%% prepare window
+if ischar(window)
+    switch window
+        case 'Hann'
+            window = hann(lSignal);
+    end
+elseif isnumeric(window) && length(window) == length(signal)
+    % NOOP, use that window
+else
+    window = hann(lSignal); % default to Hann if none specified
+end
 
 %% compute periodogram
-[ Power, W ] = periodogram(signal); % where W in rad/sample
+[ Power, W ] = periodogram(signal, window, lSignal); % W in rad/sample
 
 %% perform unit conversion
 % see http://en.wikipedia.org/w/index.php?oldid=498928359
