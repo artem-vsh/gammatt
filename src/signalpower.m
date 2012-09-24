b76e22a5da5bd1929fb12ef457e3cd98f390a725
Artem Shitov <artem.shitov@wikimart.ru>
2012-09-24 16:04:10 +0400

    add frequency sampling support

--- src/signalpower.m
+++ src/signalpower.m
@@ -1,12 +1,15 @@
-function [ PowerByFrequency ] = signalpower( signal, Fs, WindowName )
+function [ PowerByFrequency ] = signalpower( signal, Fs, WindowName, DowntoFrequency )
 %POWER Compute classical square power periodogram
 lSignal = length(signal);
 if nargin < 3
     WindowName = 'Hann';
 end
+if nargin < 4
+    DowntoFrequency = Fs / lSignal;
+end
 
 %% compute periodogram
-Welch = spectrum.welch(WindowName);
+Welch = spectrum.welch(WindowName, Fs / DowntoFrequency);
 Hmss = msspectrum(Welch, signal, 'NFFT', lSignal, 'Fs', Fs);
 
 %% return
