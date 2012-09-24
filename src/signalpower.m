e89d6dae53f4e62f6dea76a7b018a428c8173c7d
Artem Shitov <artem.shitov@wikimart.ru>
2012-09-24 15:59:00 +0400

    restore custom windowing support

--- src/signalpower.m
+++ src/signalpower.m
@@ -1,24 +1,12 @@
-function [ PowerByFrequency ] = signalpower( signal, Fs, window )
+function [ PowerByFrequency ] = signalpower( signal, Fs, WindowName )
 %POWER Compute classical square power periodogram
 lSignal = length(signal);
 if nargin < 3
-    window = [];
-end
-
-%% prepare window
-if ischar(window)
-    switch window
-        case 'Hann'
-            window = hann(lSignal);
-    end
-elseif isnumeric(window) && length(window) == length(signal)
-    % NOOP, use that window
-else
-    window = hann(lSignal); % default to Hann if none specified
+    WindowName = 'Hann';
 end
 
 %% compute periodogram
-Welch = spectrum.welch('Hann');
+Welch = spectrum.welch(WindowName);
 Hmss = msspectrum(Welch, signal, 'NFFT', lSignal, 'Fs', Fs);
 
 %% return
